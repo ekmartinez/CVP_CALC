827b804fd7a3edfdee99e2996262ef06915d68c5
Erick Martinez <analyst_martinez@outlook.com>
2022-10-23 18:19:40 -0400

    Add additional functions

--- cvp_calc.py
+++ cvp_calc.py
@@ -4,6 +4,11 @@ class Cvp_calc:
         self.vc = vc
         self.fc = fc
 
+    def break_even_units(self):
+        cm = (self.sls - self.vc)
+        result = self.fc / cm
+        return result
+
     def break_even_dollars(self):
         cm_pct = (self.sls - self.vc) / self.sls
         result = self.fc / cm_pct
@@ -15,3 +20,9 @@ class Cvp_calc:
         break_even = (self.fc + ni) / cm
         return break_even
 
+    def bev_units_ni(self, ni):
+        """Returns break even dollars for specific net income"""
+        cm = self.sls - self.vc
+        cm_pct = cm / self.sls
+        break_even = (self.fc + ni) / cm_pct
+        return break_even
