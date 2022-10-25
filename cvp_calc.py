e054219451037902929677378297a6ec4f4eeec2
Erick Martinez <analyst_martinez@outlook.com>
2022-10-24 20:00:17 -0400

    To add tax effect functions

--- cvp_calc.py
+++ cvp_calc.py
@@ -21,10 +21,23 @@ class Cvp_calc:
         cm = self.sls - self.vc
         break_even = (self.fc + ni) / cm
         return break_even
+    
+    def bev_units_ni_tx(self, ni, tx):
+        """Returns break even units for specific net income including tax impact"""
+        cm = self.sls - self.vc
+        break_even = (self.fc + (ni / (1 - tx))) / cm
+        return break_even
 
     def bev_dollars_ni(self, ni):
         """Returns break even dollars for specific net income"""
         cm = self.sls - self.vc
         cm_pct = cm / self.sls
         break_even = (self.fc + ni) / cm_pct
+        return break_even
+
+    def bev_dollars_ni_tx(self, ni, tx):
+        """Returns break even dollars for specific net income including tax impact"""
+        cm = self.sls - self.vc
+        cm_pct = cm / self.sls
+        break_even = (self.fc + (ni / (1 - tx))) / cm_pct
         return break_even
